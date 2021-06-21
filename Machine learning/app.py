4a4d3f3db095fa4ac84acbfaa406b2c6ca2a4efb
ushbig <62624786+ushbig@users.noreply.github.com>
2021-06-21 01:30:16 +0100

    Add files via upload

---
+++ Machine learning/app.py
@@ -0,0 +1,8 @@
+from sklearn.naive_bayes import GaussianNB
+import numpy as np
+X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
+Y = np.array([1, 1, 1, 2, 2, 2])
+
+clf = GaussianNB()
+clf.fit(X, Y)
+GaussianNB()
