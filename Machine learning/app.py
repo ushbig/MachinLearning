dbe26b0a05faa9c620b56c9a764c95a052073eec
ushbig <62624786+ushbig@users.noreply.github.com>
2021-06-21 01:33:45 +0100

    Delete Machine learning directory

--- Machine learning/app.py
+++
@@ -1,8 +0,0 @@
-from sklearn.naive_bayes import GaussianNB
-import numpy as np
-X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
-Y = np.array([1, 1, 1, 2, 2, 2])
-
-clf = GaussianNB()
-clf.fit(X, Y)
-GaussianNB()
